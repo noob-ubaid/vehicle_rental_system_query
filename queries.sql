4e025f08da128dbbdf0fb81355e7f024e7c75b29
noob-ubaid <ubaidurrahman661@gmail.com>
2025-12-23 14:46:11 +0600

    solved second query

--- queries.sql
+++ queries.sql
@@ -10,3 +10,24 @@ from
   bookings as b
   join users as u on b.user_id = u.user_id
   join vehicles as v on b.vehicle_id = v.vehicle_id
+
+-- second query
+select
+  v.vehicle_id,
+  v.vehicle_name,
+  v.type,
+  v.model,
+  v.registration_number,
+  v.rental_price,
+  v.status
+from
+  vehicles as v
+where
+  not exists (
+    select
+      *
+    from
+      bookings as b
+    where
+      b.vehicle_id = v.vehicle_id
+  )
